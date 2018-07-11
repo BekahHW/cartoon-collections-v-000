65ba7ccced83b46f140c1410c47c9d121cea9b68
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-11 17:02:20 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def summon_captain_planet(planeteers)
 end
 
 def long_planeteer_calls(array)
-  array.all? {|word| word.length >= 5}
+  array.any? {|word| word.length >= 5}
 end
 
 def find_the_cheese# code an argument here

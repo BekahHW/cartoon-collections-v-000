2d288484903c0997e60fa7d8a3eedd4560d1baf0
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-11 17:05:03 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(array)
   array.any? {|word| word.length >= 5}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.include {|cheese| 
 end
